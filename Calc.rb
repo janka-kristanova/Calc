4454654170d7f5e49fdaa26a0c0af49f9ebf5a14
wilonwonder <wilonwonder@users.noreply.github.com>
2015-01-20 13:19:18 +0100

    Update Calc.rb
    Added operations: exponent, nthroot and modulus
    Added support for repetitive calculation. program ends with "exit" command now.

--- Calc.rb
+++ Calc.rb
@@ -1,29 +1,37 @@
 puts "Welcome to calculator.  Please enter an equation in the following format:"
-puts "Number1 Operation(+ - * /) Number 2"
+puts "Number1 Operation Number2"
+puts "Valid operations: +, -, *, /, ** (exponent), % (modulus), rad (nth root)"
 puts "Example: 4 + 7"
-
-print"I want to calculate: "
-entry = gets.chomp
-
-entry_array = entry.split
-
-num_one = entry_array[0].to_f
-num_two = entry_array[2].to_f
-
-case 
-when entry_array[1]=="+"
-	result = num_one + num_two
-when entry_array[1]=="-"
-	result = num_one - num_two
-when entry_array[1]=="*"
-	result = num_one * num_two
-when entry_array[1]=="/"
-	result = num_one / num_two
-else
-	result = "INVALID ENTRY, make sure to use a space between Numbers and Operators"
-end
-
-puts result
-
+begin
+  print"I want to calculate: "
+  entry = gets.chomp
+  entry_array = entry.split
+  num_one = entry_array[0].to_f
+  num_two = entry_array[2].to_f
+  
+  case 
+  when entry_array[1]=="+"
+  	result = num_one + num_two
+  when entry_array[1]=="-"
+  	result = num_one - num_two
+  when entry_array[1]=="*"
+  	result = num_one * num_two
+  when entry_array[1]=="/"
+  	result = num_one / num_two
+  when entry_array[1]=="**"
+  	result = num_one ** num_two
+  when entry_array[1]=="rad"
+  	result = num_one ** (1.0 / num_two)
+  when entry_array[1]=="%"
+  	result = num_one % num_two
+  else
+    result = "INVALID ENTRY, make sure to use a space between Numbers and Operators."
+  end
+  
+  puts "Result: #{result}"
+  puts ""
+  puts "Do you want to continue using calculator? yes - [Enter], No - \"exit\""
+  entry = gets.chomp
+end until entry == "exit"
 
 
